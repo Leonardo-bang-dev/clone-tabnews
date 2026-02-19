a9fbc966217befbb075f6da0475a27f729e5e91d
LeonardoBang <109624021+Leonardo-bang-dev@users.noreply.github.com>
2026-02-19 18:51:00 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "São acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersion.rows[0].server_version;
+
+  const maxConnections = await database.query("SHOW max_connections");
+  const maxConnectionsValue = maxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnections = await database.query({
+    text: "SELECT COUNT(*)::int used_connections FROM pg_stat_activity WHERE datname=$1",
+    values: [databaseName],
+  });
+  const openedConnectionsValue = openedConnections.rows[0].used_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: openedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,26 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  // Pegando responseBody
+  const responseBody = await response.json();
+
+  // Confere se valores estão definido
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+
+  // Confere se data confere
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  // Confere se a versão banco esta correta
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+
+  // Confere se max_connections esta correta
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  // Confere se opened_connections esta retornando tipo certo
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
